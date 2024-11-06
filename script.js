fe9ff57565127ae8bcacdcf144bce7f70227f307
dalessandrosantos <dalessandrosantos83@gmail.com>
2024-11-06 18:29:03 -0300

    ajustando javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+/*
+
+    Lógica de Programação
+
+    [x] Pegar os dados do Input, quando o botão for clicado
+    [x] Ir até o servidor, e trazer os produtos
+    [] Colocar os produtos na tela
+    [] Criar o grá fico de preços
+
+*/ 
+
+const searchForm = document.querySelector('.search-form')
+const productList = document.querySelector('.product-list')
+
+searchForm.addEventListener('submit', async function(event) {
+    event.preventDefault()
+    const inputValue = event.target[0].value
+
+    const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${inputValue}`)
+    const products = (await data.json()).results.slice(0, 10)
+
+    displayItens(products)
+    
+})
+
+function displayItens(products) {
+    console.log(products)
+    productList.innerHTML = products.map(product => `
+        <div class="product-card">
+            <img src="${product.thumbnail}">
+
+        </div>
+        
+        
+        `)
+}
